ee5aa367a7c0254a3a0a9203d6f8656949505806
이문빈 <3412mb@gmail.com>
2023-03-27 15:28:57 +0900

    Time: 32 ms (82.01%), Space: 71.3 MB (85.35%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,12 +1,12 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *int val;
- *ListNode * next;
- *ListNode() : val(0), next(nullptr) {}
- *ListNode(int x) : val(x), next(nullptr) {}
- *ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution
 {
@@ -21,7 +21,7 @@ class Solution
             {
                 int v1 = 0;
                 int v2 = 0;
-                
+
                 if (l1)
                 {
                     v1 = l1->val;
@@ -32,14 +32,14 @@ class Solution
                     v2 = l2->val;
                     l2 = l2->next;
                 }
-                
+
                 int sum = v1 + v2 + carry;
                 carry = sum / 10;
                 sum %= 10;
                 pNode->val = sum;
-                
-                if (!l1 && !l2 && !carry) return head;
-                
+
+                if (!l1 && !l2 && !carry) break;
+
                 pNode->next = new ListNode();
                 pNode = pNode->next;
             }
