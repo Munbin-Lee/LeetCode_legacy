671bd95d6177f31e77c03984af2286d38e5b8b27
이문빈 <3412mb@gmail.com>
2023-03-28 14:29:57 +0900

    Time: 7 ms (20.23%), Space: 9.2 MB (36.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,25 +3,13 @@ class Solution
     public:
         void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
         {
-            vector<int> res = vector<int> (nums1.size());
+            int c = m + n - 1;
+            int c1 = m - 1;
+            int c2 = n - 1;
 
-            int c = 0;
-            int c1 = 0;
-            int c2 = 0;
-
-            while (c1 < m && c2 < n)
+            while (c2 >= 0)
             {
-                res[c++] = (nums1[c1] < nums2[c2]) ? nums1[c1++] : nums2[c2++];
+                nums1[c--] = (c1 < 0 || nums2[c2] >= nums1[c1]) ? nums2[c2--] : nums1[c1--];
             }
-            while (c1 < m)
-            {
-                res[c++] = nums1[c1++];
-            }
-            while (c2 < n)
-            {
-                res[c++] = nums2[c2++];
-            }
-
-            nums1 = res;
         }
 };
