ee505f087225b97b93700c47b44654a4e43344f3
이문빈 <3412mb@gmail.com>
2023-03-29 13:43:16 +0900

    Time: 12 ms (74.77%), Space: 15.6 MB (58.27%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,8 +3,9 @@ class Solution
     public:
         vector<int> twoSum(vector<int> &numbers, int target)
         {
+            int n = numbers.size();
             int lo = 0;
-            int hi = numbers.size() - 1;
+            int hi = n - 1;
             int sum = numbers[lo] + numbers[hi];
 
             while (sum != target)
@@ -13,9 +14,6 @@ class Solution
                 sum = numbers[lo] + numbers[hi];
             }
 
-            return {
-                lo + 1,
-                hi + 1
-            };
+            return { lo + 1, hi + 1 };
         }
 };
