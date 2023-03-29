776f777e7ac926aa990d4892438e5a06c081042a
이문빈 <3412mb@gmail.com>
2023-03-29 15:42:48 +0900

    Time: 1252 ms (30.44%), Space: 27.8 MB (49.62%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,7 +4,7 @@ class Solution
         int binarySearch(vector<int> &v, int num, int lo, int hi)
         {
             while (lo <= hi) {
-                int md = (lo+hi)/2;
+                int md = (lo + hi) / 2;
                 if (v[md] == num) return v[md];
                 (v[md] < num)? lo = md + 1: hi = md - 1;
             }
@@ -15,9 +15,9 @@ class Solution
         {
             int n = nums.size();
             sort(nums.begin(), nums.end());
-
             vector<vector<int>> res;
-            set<tuple<int, int, int>> st;
+            map<pair<int, int>, bool> visited;
+            
             for (int i = 0; i < n - 2; i++)
             {
                 for (int j = i + 1; j < n - 1; j++)
@@ -25,13 +25,10 @@ class Solution
                     int sum = nums[i] + nums[j];
                     int k = binarySearch(nums, -sum, j + 1, n - 1);
                     if (k == -1) continue;
-                    st.emplace(nums[i], nums[j], k);
+                    if (visited[{nums[i], nums[j]}]) continue;
+                    visited[{nums[i], nums[j]}] = true;
+                    res.emplace_back(vector<int> {nums[i], nums[j], k});
                 }
-            }
-            
-            for(auto i : st) {
-                auto [ a, b, c ] = i;
-                res.emplace_back(vector<int> {a, b, c});
             }
             return res;
         }
