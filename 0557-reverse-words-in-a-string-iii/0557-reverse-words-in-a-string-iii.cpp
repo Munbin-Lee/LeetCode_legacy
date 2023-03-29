4c7cc7cc59a61b85e63b2d014dc285e5f77acc52
이문빈 <3412mb@gmail.com>
2023-03-29 14:39:55 +0900

    Time: 30 ms (11.28%), Space: 10.9 MB (39.08%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -3,10 +3,11 @@ class Solution
     public:
         string reverseWords(string s)
         {
+            int n = s.size();
             stack<char> st;
             string res;
 
-            for (int i = 0; i < s.size(); i++)
+            for (int i = 0; i < n; i++)
             {
                 if (s[i] != ' ')
                 {
