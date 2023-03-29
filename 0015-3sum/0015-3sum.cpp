dbdd93f5b66a2c9e079d63296eee1145b9675598
이문빈 <3412mb@gmail.com>
2023-03-29 15:24:51 +0900

    Time: 1449 ms (17.16%), Space: 27.7 MB (49.62%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:    
+        int binarySearch(vector<int> &v, int num, int lo, int hi)
+        {
+            while (lo <= hi) {
+                int md = (lo+hi)/2;
+                if (v[md] == num) return v[md];
+                (v[md] < num)? lo = md + 1: hi = md - 1;
+            }
+            return -1;
+        }
+    
+        auto threeSum(vector<int> &nums)
+        {
+            int n = nums.size();
+            sort(nums.begin(), nums.end());
+
+            vector<vector<int>> res;
+            map<tuple<int, int, int>, bool> visited;
+            for (int i = 0; i < n - 2; i++)
+            {
+                for (int j = i + 1; j < n - 1; j++)
+                {
+                    int sum = nums[i] + nums[j];
+                    int k = binarySearch(nums, -sum, j + 1, n - 1);
+                    if (k == -1) continue;
+                    if (visited[{nums[i], nums[j], k}]) continue;
+                    visited[{nums[i], nums[j], k}] = true;
+                    res.emplace_back(vector<int>{ nums[i], nums[j], k});
+                }
+            }
+            return res;
+        }
+};
