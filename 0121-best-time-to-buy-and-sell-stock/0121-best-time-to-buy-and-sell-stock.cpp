eb27730d6b4a33670005ec66757f4cc91e4c7048
이문빈 <3412mb@gmail.com>
2023-03-29 13:35:41 +0900

    Time: 123 ms (90.95%), Space: 93.3 MB (57.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,14 +3,13 @@ class Solution
     public:
         int maxProfit(vector<int> &prices)
         {
-            priority_queue<int, vector < int>, greater < >> pq;
-            pq.emplace(987654321);
-
+            int minPrice = 987654321;
             int maxProfit = 0;
+
             for (int i: prices)
             {
-                maxProfit = max(maxProfit, i - pq.top());
-                pq.emplace(i);
+                maxProfit = max(maxProfit, i - minPrice);
+                minPrice = min(minPrice, i);
             }
             return maxProfit;
         }
