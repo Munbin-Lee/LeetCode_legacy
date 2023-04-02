04e1fd005ec7d552979ba4a3f6b6ee2bd704f9d5
이문빈 <3412mb@gmail.com>
2023-04-02 22:14:48 +0900

    Time: 0 ms (100.00%), Space: 7.9 MB (36.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,19 +3,13 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
-        if (n == 2) return max(nums[0], nums[1]);
         
         vector<int> memo(n);
         memo[0] = nums[0];
-        memo[1] = nums[1];
-        memo[2] = nums[0] + nums[2];
-        int maxMoney = max(memo[1], memo[2]);
-        
-        for (int i = 3; i < n; i++) {
-            memo[i] = max(memo[i - 2], memo[i - 3]);
-            memo[i] += nums[i];
-            maxMoney = max(maxMoney, memo[i]);
+        memo[1] = max(memo[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            memo[i] = max(memo[i - 1], memo[i - 2] + nums[i]);
         }
-        return maxMoney;
+        return memo[n - 1];
     }
 };
