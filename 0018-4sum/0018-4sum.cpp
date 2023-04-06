9d84cfcf212b6a403685173ec5148a9bc70f00f5
이문빈 <3412mb@gmail.com>
2023-04-06 15:22:01 +0900

    Time: 1554 ms (5.01%), Space: 184.3 MB (5.08%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        
+        for (int i = 0; i + 3 < n; i++) {
+            for(int j = i + 1; j + 2 < n; j++) {
+                int lo = j + 1;
+                int hi = n - 1;
+                while (lo < hi) {
+                    long long int sum = (long long int) nums[i] + nums[j] + nums[lo] + nums[hi];
+                    if (sum == target) {
+                        res.emplace_back(
+                            vector<int>{nums[i], nums[j], nums[lo], nums[hi]}
+                        );
+                    }
+                    (sum < target)? lo++: hi--;
+                }
+            }
+        }
+        sort(res.begin(), res.end());
+        res.erase(unique(res.begin(), res.end()), res.end());
+        return res;
+    }
+};
