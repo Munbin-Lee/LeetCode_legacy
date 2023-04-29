bb6621f1ade82f6a9c2fb17361fcd176aa4595f8
이문빈 <3412mb@gmail.com>
2023-04-29 22:43:27 +0900

    Time: 0 ms (100.00%), Space: 8.6 MB (93.72%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int cnt = 0;
+        int i = 0;
         
-        for (int i : nums) {
-            if (i == val) cnt++;
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
+            }
         }
         
-        int maxLen = n - cnt;
-        int curLen = 0;
-        int lo = 0;
-        int hi = 0;
-        
-        while (curLen < maxLen) {
-            while (nums[hi] == val) hi++;
-            if (nums[lo] == val) swap(nums[lo], nums[hi]);
-            lo++;
-            hi = lo;
-            curLen++;
-        }
-        
-        return maxLen;
+        return i;
     }
 };
