a2800387fc98e051338aa67f2d2b1552e6344b50
이문빈 <3412mb@gmail.com>
2023-04-29 22:56:47 +0900

    Time: 4 ms (43.18%), Space: 6 MB (77.91%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x, n);
+    double myPow(double x, long long int n) {
+        if (n < 0) return myPow(1.0 / x, -n);
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == 2) return x * x;
+        if (n % 2) return x * myPow(x, n - 1);
+        return myPow(myPow(x, n / 2), 2);
     }
 };
